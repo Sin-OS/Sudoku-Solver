ae9c69ebb083e7f51f97d140f09d9cf739d8599d
SinOS <yanis.y.b.y@gmail.com>
2024-12-26 19:26:00 +0100

    Added main math file
    - where the math ocures.

---
+++ SudoSolver/src/meths.java
@@ -0,0 +1,12 @@
+public class meths {
+    int[][] sudoTable = new int[9][9];
+    int length = sudoTable.length;
+
+    public void enterNums(int[][] table, int lng){
+        for(int i = 1; i < lng; i++){
+            for(int j = 1; j < lng; j++){
+                
+            }
+        }
+    }
+}
